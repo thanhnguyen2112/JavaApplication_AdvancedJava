a9b293d4e2b0342a211e916f500b695788015f9c
thanhnguyen2112 <72983910+thanhnguyen2112@users.noreply.github.com>
2021-07-29 16:39:40 +0700

    coll

--- src/Collections/Main.java
+++ src/Collections/Main.java
@@ -9,9 +9,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		CollectionDemo.show();
-		
+		System.out.println("-----------------------");
 		ListDemo.show();
-		
+		System.out.println("-----------------------");
 		List<Customer> customers = new ArrayList<>();
 		customers.add(new Customer("B", "e3"));
 		customers.add(new Customer("A", "e2"));
@@ -23,12 +23,17 @@ public class Main {
 		
 		System.out.println(customers);
 		
+		System.out.println("-----------------------");
+		
 		
 		QueueDemo.show();
+		System.out.println("-----------------------");
 		
 		SetDemo.show();
+		System.out.println("-----------------------");
 		
 		MapDemo.show();
+		System.out.println("-----------------------");
 	}
 
 }
